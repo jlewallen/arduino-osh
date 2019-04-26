c581bf79f9dc21ce8edfb46ed12bd03431e7cdf3
Jacob Lewallen <jlewallen@gmail.com>
2019-04-26 14:05:43 -0700

    Formatting.

--- os.cpp
+++ os.cpp
@@ -22,32 +22,32 @@
 #include "os.h"
 
 enum os_task_status {
-	OS_TASK_STATUS_IDLE = 1,
-	OS_TASK_STATUS_ACTIVE,
+    OS_TASK_STATUS_IDLE = 1,
+    OS_TASK_STATUS_ACTIVE,
 };
 
 struct os_task {
-	/* The stack pointer (sp) has to be the first element as it is located
-	   at the same address as the structure itself (which makes it possible
-	   to locate it safely from assembly implementation of PendSV_Handler).
-	   The compiler might add padding between other structure elements. */
-	volatile uint32_t sp;
-	void (*handler)(void *p_params);
-	void *p_params;
-	volatile enum os_task_status status;
+    /* The stack pointer (sp) has to be the first element as it is located
+       at the same address as the structure itself (which makes it possible
+       to locate it safely from assembly implementation of PendSV_Handler).
+       The compiler might add padding between other structure elements. */
+    volatile uint32_t sp;
+    void (*handler)(void *p_params);
+    void *p_params;
+    volatile enum os_task_status status;
 };
 
 static enum {
-	OS_STATE_DEFAULT = 1,
-	OS_STATE_INITIALIZED,
-	OS_STATE_TASKS_INITIALIZED,
-	OS_STATE_STARTED,
+    OS_STATE_DEFAULT = 1,
+    OS_STATE_INITIALIZED,
+    OS_STATE_TASKS_INITIALIZED,
+    OS_STATE_STARTED,
 } m_state = OS_STATE_DEFAULT;
 
 static struct {
-	struct os_task tasks[OS_CONFIG_MAX_TASKS];
-	volatile uint32_t current_task;
-	uint32_t size;
+    struct os_task tasks[OS_CONFIG_MAX_TASKS];
+    volatile uint32_t current_task;
+    uint32_t size;
 } m_task_table;
 
 volatile struct os_task *os_curr_task;
@@ -55,100 +55,100 @@ volatile struct os_task *os_next_task;
 volatile bool os_initialized = false;
 
 static void task_finished(void) {
-	/* This function is called when some task handler returns. */
-	volatile uint32_t i = 0;
-	while (1) {
-		i++;
+    /* This function is called when some task handler returns. */
+    volatile uint32_t i = 0;
+    while (1) {
+        i++;
     }
 }
 
 bool os_initialize(void) {
-	if (m_state != OS_STATE_DEFAULT) {
-		return false;
+    if (m_state != OS_STATE_DEFAULT) {
+        return false;
     }
 
-	memset(&m_task_table, 0, sizeof(m_task_table));
-	m_state = OS_STATE_INITIALIZED;
+    memset(&m_task_table, 0, sizeof(m_task_table));
+    m_state = OS_STATE_INITIALIZED;
 
-	return true;
+    return true;
 }
 
 bool os_task_initialize(void (*handler)(void *p_params), void *p_task_params, uint32_t *p_stack, size_t stack_size) {
-	if (m_state != OS_STATE_INITIALIZED && m_state != OS_STATE_TASKS_INITIALIZED) {
-		return false;
+    if (m_state != OS_STATE_INITIALIZED && m_state != OS_STATE_TASKS_INITIALIZED) {
+        return false;
     }
 
-	if (m_task_table.size >= OS_CONFIG_MAX_TASKS - 1) {
-		return false;
+    if (m_task_table.size >= OS_CONFIG_MAX_TASKS - 1) {
+        return false;
     }
 
-	if ((stack_size % sizeof(uint32_t)) != 0) /* TODO: Use assert? */ {
-		return false;
+    if ((stack_size % sizeof(uint32_t)) != 0) /* TODO: Use assert? */ {
+        return false;
     }
 
-	uint32_t stack_offset = (stack_size / sizeof(uint32_t));
+    uint32_t stack_offset = (stack_size / sizeof(uint32_t));
 
-	/* Initialize the task structure and set SP to the top of the stack
-	   minus 16 words (64 bytes) to leave space for storing 16 registers: */
-	struct os_task *p_task = &m_task_table.tasks[m_task_table.size];
-	p_task->handler = handler;
-	p_task->p_params = p_task_params;
-	p_task->sp = (uint32_t)(p_stack + stack_offset - 16);
-	p_task->status = OS_TASK_STATUS_IDLE;
+    /* Initialize the task structure and set SP to the top of the stack
+       minus 16 words (64 bytes) to leave space for storing 16 registers: */
+    struct os_task *p_task = &m_task_table.tasks[m_task_table.size];
+    p_task->handler = handler;
+    p_task->p_params = p_task_params;
+    p_task->sp = (uint32_t)(p_stack + stack_offset - 16);
+    p_task->status = OS_TASK_STATUS_IDLE;
 
-	/* Save init. values of registers which will be restored on exc. return:
-	   - XPSR: Default value (0x01000000)
-	   - PC: Point to the handler function
-	   - LR: Point to a function to be called when the handler returns
-	   - R0: Point to the handler function's parameter */
-	p_stack[stack_offset - 1] = 0x01000000;
-	p_stack[stack_offset - 2] = (uint32_t)handler;
-	p_stack[stack_offset - 3] = (uint32_t)&task_finished;
-	p_stack[stack_offset - 8] = (uint32_t)p_task_params;
+    /* Save init. values of registers which will be restored on exc. return:
+       - XPSR: Default value (0x01000000)
+       - PC: Point to the handler function
+       - LR: Point to a function to be called when the handler returns
+       - R0: Point to the handler function's parameter */
+    p_stack[stack_offset - 1] = 0x01000000;
+    p_stack[stack_offset - 2] = (uint32_t)handler;
+    p_stack[stack_offset - 3] = (uint32_t)&task_finished;
+    p_stack[stack_offset - 8] = (uint32_t)p_task_params;
 
 #ifdef OS_CONFIG_DEBUG
-	uint32_t base = (m_task_table.size + 1) * 1000;
-	p_stack[stack_offset - 4] = base + 12;  /* R12 */
-	p_stack[stack_offset - 5] = base + 3;   /* R3  */
-	p_stack[stack_offset - 6] = base + 2;   /* R2  */
-	p_stack[stack_offset - 7] = base + 1;   /* R1  */
-	/* p_stack[stack_offset-8] is R0 */
-	p_stack[stack_offset - 9] = base + 7;   /* R7  */
-	p_stack[stack_offset - 10] = base + 6;  /* R6  */
-	p_stack[stack_offset - 11] = base + 5;  /* R5  */
-	p_stack[stack_offset - 12] = base + 4;  /* R4  */
-	p_stack[stack_offset - 13] = base + 11; /* R11 */
-	p_stack[stack_offset - 14] = base + 10; /* R10 */
-	p_stack[stack_offset - 15] = base + 9;  /* R9  */
-	p_stack[stack_offset - 16] = base + 8;  /* R8  */
+    uint32_t base = (m_task_table.size + 1) * 1000;
+    p_stack[stack_offset - 4] = base + 12;  /* R12 */
+    p_stack[stack_offset - 5] = base + 3;   /* R3  */
+    p_stack[stack_offset - 6] = base + 2;   /* R2  */
+    p_stack[stack_offset - 7] = base + 1;   /* R1  */
+    /* p_stack[stack_offset-8] is R0 */
+    p_stack[stack_offset - 9] = base + 7;   /* R7  */
+    p_stack[stack_offset - 10] = base + 6;  /* R6  */
+    p_stack[stack_offset - 11] = base + 5;  /* R5  */
+    p_stack[stack_offset - 12] = base + 4;  /* R4  */
+    p_stack[stack_offset - 13] = base + 11; /* R11 */
+    p_stack[stack_offset - 14] = base + 10; /* R10 */
+    p_stack[stack_offset - 15] = base + 9;  /* R9  */
+    p_stack[stack_offset - 16] = base + 8;  /* R8  */
 #endif /* OS_CONFIG_DEBUG */
 
-	m_state = OS_STATE_TASKS_INITIALIZED;
-	m_task_table.size++;
+    m_state = OS_STATE_TASKS_INITIALIZED;
+    m_task_table.size++;
 
-	return true;
+    return true;
 }
 
 bool os_start(void) {
-	if (m_state != OS_STATE_TASKS_INITIALIZED) {
-		return false;
+    if (m_state != OS_STATE_TASKS_INITIALIZED) {
+        return false;
     }
 
-	NVIC_SetPriority(PendSV_IRQn, 0xff);
-	NVIC_SetPriority(SysTick_IRQn, 0x00);
+    NVIC_SetPriority(PendSV_IRQn, 0xff);
+    NVIC_SetPriority(SysTick_IRQn, 0x00);
 
-	os_curr_task = &m_task_table.tasks[m_task_table.current_task];
-	m_state = OS_STATE_STARTED;
+    os_curr_task = &m_task_table.tasks[m_task_table.current_task];
+    m_state = OS_STATE_STARTED;
 
-	__set_PSP(os_curr_task->sp + 64); /* Set PSP to the top of task's stack */
-	__set_CONTROL(0x03); /* Switch to Unprivilleged Thread Mode with PSP */
-	__ISB(); /* Execute ISB after changing CONTORL (recommended) */
+    __set_PSP(os_curr_task->sp + 64); /* Set PSP to the top of task's stack */
+    __set_CONTROL(0x03); /* Switch to Unprivilleged Thread Mode with PSP */
+    __ISB(); /* Execute ISB after changing CONTORL (recommended) */
 
     os_initialized = true;
 
-	os_curr_task->handler(os_curr_task->p_params);
+    os_curr_task->handler(os_curr_task->p_params);
 
-	return true;
+    return true;
 }
 
 extern "C" {
@@ -192,38 +192,38 @@ void PendSV_Handler() {
         "cpsid	i\n"
 
         /*
-        Exception frame saved by the NVIC hardware onto stack:
-        +------+
-        |      | <- SP before interrupt (orig. SP)
-        | xPSR |
-        |  PC  |
-        |  LR  |
-        |  R12 |
-        |  R3  |
-        |  R2  |
-        |  R1  |
-        |  R0  | <- SP after entering interrupt (orig. SP + 32 bytes)
-        +------+
+          Exception frame saved by the NVIC hardware onto stack:
+          +------+
+          |      | <- SP before interrupt (orig. SP)
+          | xPSR |
+          |  PC  |
+          |  LR  |
+          |  R12 |
+          |  R3  |
+          |  R2  |
+          |  R1  |
+          |  R0  | <- SP after entering interrupt (orig. SP + 32 bytes)
+          +------+
 
-        Registers saved by the software (PendSV_Handler):
-        +------+
-        |  R7  |
-        |  R6  |
-        |  R5  |
-        |  R4  |
-        |  R11 |
-        |  R10 |
-        |  R9  |
-        |  R8  | <- Saved SP (orig. SP + 64 bytes)
-        +------+
+          Registers saved by the software (PendSV_Handler):
+          +------+
+          |  R7  |
+          |  R6  |
+          |  R5  |
+          |  R4  |
+          |  R11 |
+          |  R10 |
+          |  R9  |
+          |  R8  | <- Saved SP (orig. SP + 64 bytes)
+          +------+
         */
 
         /* Save registers R4-R11 (32 bytes) onto current PSP (process stack
-        pointer) and make the PSP point to the last stacked register (R8):
-        - The MRS/MSR instruction is for loading/saving a special registers.
-        - The STMIA inscruction can only save low registers (R0-R7), it is
-            therefore necesary to copy registers R8-R11 into R4-R7 and call
-            STMIA twice. */
+           pointer) and make the PSP point to the last stacked register (R8):
+           - The MRS/MSR instruction is for loading/saving a special registers.
+           - The STMIA inscruction can only save low registers (R0-R7), it is
+           therefore necesary to copy registers R8-R11 into R4-R7 and call
+           STMIA twice. */
         "mrs	r0, psp\n"
         "subs	r0, #16\n"
         "stmia	r0!,{r4-r7}\n"
@@ -246,8 +246,8 @@ void PendSV_Handler() {
         "ldr	r0, [r1]\n"
 
         /* Load registers R4-R11 (32 bytes) from the new PSP and make the PSP
-        point to the end of the exception stack frame. The NVIC hardware
-        will restore remaining registers after returning from exception): */
+           point to the end of the exception stack frame. The NVIC hardware
+           will restore remaining registers after returning from exception): */
         "ldmia	r0!,{r4-r7}\n"
         "mov	r8, r4\n"
         "mov	r9, r5\n"
@@ -263,7 +263,7 @@ void PendSV_Handler() {
         "cpsie	i\n"
 
         "bx	r0\n"
-    );
+        );
 }
 
 }
